bb1e504281306f426dbe70324efce9122da0232c
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2026-03-30 09:37:11 -0700

    completed

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
       <>
         <Link href="/" className="flex items-center gap-2 group">
-          <div className="md:h-9 md:w-9 h-7 w-7 bg-purple-900 rounded-full flex items-center justify-center font-black text-white group-hover:bg-blue-600 transition-colors">
+          <div className="md:h-9 md:w-9 h-7 w-7 bg-purple-900 rounded-full flex items-center justify-center font-black text-white transition-colors">
             N
           </div>
           <span className="md:text-2xl text-lg font-black text-slate-900 tracking-tighter">
